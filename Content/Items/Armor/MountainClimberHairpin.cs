a0ffcd7dd5444b3db5b5584d08dec56276ac7820
squi1-rasb <TacoLlamaGuy@gmail.com>
2023-05-07 18:41:39 -0700

    Added Mountain climber vanity

---
+++ Content/Items/Armor/MountainClimberHairpin.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TidesOfTime.Content.Items.Armor
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class MountainClimberHairpin : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 22; 
+            Item.height = 22; 
+            Item.rare = ItemRarityID.Green; 
+        }
+    }
+}
